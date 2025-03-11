0c45ec9731d668899d9b59fabd0d836624c8cfdc
Tanishq Harchandani <tharchandani93@gmail.com>
2025-03-11 17:28:18 +0530

    h

--- CyberSpy/settings.py
+++ CyberSpy/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "main_app/static",
-    "/var/www/static/",
+    "main_app/static/",
 ]
 
 # Default primary key field type
