9edaed334b3bf3a0e051f323eb5950e94448965f
Tanishq Harchandani <tharchandani93@gmail.com>
2025-03-11 21:04:37 +0530

    models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ContactU(models.Model):
+    first_name= models.CharField(max_length=255)
+    last_name= models.CharField(max_length=255)
+    emaill= models.CharField(max_length=255)
+    message= models.TextField(max_length=255)
+
+class mail(models.Model):
+    email2= models.TextField(max_length=255)
--- main_app/views.py
+++ main_app/views.py
@@ -2,31 +2,47 @@ from django.shortcuts import redirect, render
 from django.core.mail import send_mail
 from django.conf import settings
 
+from main_app.models import ContactU
+
 
 # Create your views here.
 def index_page(request):
     
     if request.method == 'POST':
+       
        first_name = request.POST.get('first_name')
        
        message = request.POST.get('message')
        last_name = request.POST.get('last_name')
        emaill = request.POST.get('emaill')
-       
+
+
+       ContactU.objects.create(
+            first_name=first_name,
+            emaill=emaill,
+            last_name=last_name,
+            message=message,
+        )
+
+
        send_mail('Contact Form',
         f"first name:- {first_name}\n last name:- {last_name}\n message:- {message}\n email:- {emaill}",
         settings.EMAIL_HOST_USER,
         ['kunaladwani1456@gmail.com'], 
         fail_silently=False)
+       
+       
     
     if request.method == 'POST':
        email2 = request.POST.get('email2')
        
-       send_mail('Contact Form',
+       send_mail('Drive In Mail',
         f"email:- {email2}",
         settings.EMAIL_HOST_USER,
         ['kunaladwani1456@gmail.com'], 
         fail_silently=False)
+       
+       
     
 
     return render(request,"index.html")
