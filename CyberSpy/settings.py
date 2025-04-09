c26216d7119bd6cfb39413c90ea82c0abcbd67da
Tanishq Harchandani <tharchandani93@gmail.com>
2025-04-09 21:43:23 +0530

    debug

--- CyberSpy/settings.py
+++ CyberSpy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8kgf@b-+=-v5^+hjw2p1rgka6wjr2%-ct!*y-&^xo(&t=l=x&d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['codroit.in']
 
